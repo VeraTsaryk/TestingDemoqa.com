45d682111452d26796295125463b150ff2fedc4c
Vera Tsaryk <vera.butouskaya@gmail.com>
2023-10-26 13:03:11 +0200

    Add a test to the login and divid the test with clicks

--- tests/book_store_application_login_test.py
+++ tests/book_store_application_login_test.py
@@ -32,3 +32,25 @@ def test_book_store_application_login_ok():
     profile = browser.find_element(By.CLASS_NAME, 'main-header')
     assert profile.text == 'Profile'
 
+
+def test_log_out():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    browser.get('https://demoqa.com/profile')
+    browser.find_element(By.CSS_SELECTOR, '#notLoggin-label > a:nth-child(1)').click()
+    user_name = browser.find_element(By.ID, 'userName')
+    login = "Ola"
+    user_name.send_keys(login)
+    password = browser.find_element(By.ID, 'password')
+    passwordName = '12345@Ola'
+    password.send_keys(passwordName)
+    login_button = browser.find_element(By.ID, 'login')
+    login_button.click()
+    profile_name = browser.find_element(By.ID, 'userName-value')
+    assert profile_name.is_displayed()
+    assert profile_name.text == 'Ola'
+    log_out_button = browser.find_element(By.CSS_SELECTOR, 'div[class = "text-right col-md-5 col-sm-12"] #submit')
+    log_out_button.click()
+    user_form = browser.find_element(By.CSS_SELECTOR, '#userForm > div:nth-child(1)')
+    assert profile_name != True
+    assert user_form.is_displayed()
--- tests/test_button_is_clickable.py
+++ tests/test_button_is_clickable.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 
 
-def test_buttons_is_clickable():
+def test_button_is_double_clickable():
     browser = webdriver.Chrome()
     browser.implicitly_wait(5)
     browser.maximize_window()
@@ -13,11 +13,26 @@ def test_buttons_is_clickable():
     action.double_click(button_double_click).perform()
     text_about_double_click = browser.find_element(By.ID, 'doubleClickMessage')
     assert text_about_double_click.is_displayed()
+
+
+def test_button_is_right_clickable():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    browser.maximize_window()
+    browser.get('https://demoqa.com/buttons')
+    action = ActionChains(browser)
     button_right_click = browser.find_element(By.ID, 'rightClickBtn')
     action.context_click(button_right_click).perform()
     text_right_click = browser.find_element(By.ID, 'rightClickMessage')
     assert text_right_click.is_displayed()
-    button_dynamic_click = browser.find_element(By.CSS_SELECTOR, 'div.col-12.mt-4.col-md-6 > div:nth-child(2) > div:nth-child(3)>button')
+
+
+ def test_button_is_clickable():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    browser.maximize_window()
+    browser.get('https://demoqa.com/buttons')
+    button_dynamic_click = browser.find_element(By.CSS_SELECTOR,'div.col-12.mt-4.col-md-6 > div:nth-child(2) > div:nth-child(3)>button')
     button_dynamic_click.click()
     text_dynamic_click = browser.find_element(By.ID, 'dynamicClickMessage')
     assert text_dynamic_click.is_displayed()
