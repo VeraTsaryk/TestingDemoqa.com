01a601e32bd516ee4702a83dff784c1534c4c4a7
Vera Tsaryk <vera.butouskaya@gmail.com>
2023-11-08 17:05:19 +0200

    Add PageObjectModal in test book application

--- PageObject/book_page.py
+++ PageObject/book_page.py
@@ -3,11 +3,16 @@ from PageObject.base_page import BasePage
 from PageObject.login_page import LoginPage
 
 
-class BookPage(BasePage, LoginPage):
+class BookPage(LoginPage):
     book = (By.ID, 'see-book-Git Pocket Guide')
     add_to_collection_button = (By.CSS_SELECTOR, 'div[class = "text-right fullButton"] #addNewRecordButton')
     user_profile = (By.CSS_SELECTOR, '.accordion :nth-child(6) > div > ul> li:nth-child(3)')
     login_form = (By.ID, 'login')
+    search_field = (By.ID, 'searchBox')
+    login_button_in_profile = (By.CSS_SELECTOR, '#notLoggin-label > a:nth-child(1)')
+    button_go_to_store = (By.ID, 'gotoStore')
+    delete_button = (By.ID, 'delete-record-undefined')
+    small_modal = (By.ID, 'closeSmallModal-ok')
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -24,5 +29,22 @@ class BookPage(BasePage, LoginPage):
     def click_login_form(self):
         self.click(self.login_form)
 
+    def click_user_profile(self):
+        user_profile_field = self.find(self.user_profile)
+        self.driver.execute_script("arguments[0].scrollIntoView()", user_profile_field)
+        self.click(self.user_profile)
 
+    def set_search_field(self,book_name):
+        self.set(self.search_field, book_name)
 
+    def click_login_button_in_profile(self):
+        self.click(self.login_button_in_profile)
+
+    def click_button_go_to_store(self):
+        self.click(self.button_go_to_store)
+
+    def click_delete_button(self):
+        self.click(self.delete_button)
+
+    def click_close_small_model(self):
+        self.click(self.small_modal)
--- tests/test_add_book_into_your_profile.py
+++ tests/test_add_book_into_your_profile.py
@@ -1,107 +1,169 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
+
+from PageObject.book_page import BookPage
+from tests.base_test import BaseTest
+from utilities.test_data import TestData
 
 
-def test_add_book_into_your_profile():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    browser.maximize_window()
-    browser.get('https://demoqa.com/books')
-    book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
-    book.click()
-    login_button = browser.find_element(By.ID, 'login')
-    login_button.click()
-    user_name = browser.find_element(By.ID, 'userName')
-    login = "Ola"
-    user_name.send_keys(login)
-    password = browser.find_element(By.ID, 'password')
-    passwordName = '12345@Ola'
-    password.send_keys(passwordName)
-    login_button = browser.find_element(By.ID, 'login')
-    login_button.click()
-    add_to_collection_button = browser.find_element(By.CSS_SELECTOR,
-                                                    'div[class = "text-right fullButton"] #addNewRecordButton')
-    add_to_collection_button.click()
-    WebDriverWait(browser, 10).until(EC.alert_is_present())
-    browser.switch_to.alert.accept()
-    user_profile = browser.find_element(By.CSS_SELECTOR, '.accordion :nth-child(6) > div > ul> li:nth-child(3)')
-    # WebDriverWait(browser, 10).until(
-    #     EC.element_to_be_clickable((By.CSS_SELECTOR, '.accordion :nth-child(6) > div > ul> li:nth-child(3)')))
-    
-    user_profile.click()
-    book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
-    assert book.is_displayed()
+class TestBookApplication(BaseTest):
+    def test_add_book_into_your_profile(self):
+        book_page = BookPage(self.driver)
+        book_page.open_page(TestData.book_url)
+        book_page.click_book()
+        book_page.click_login_form()
+        book_page.log_into_application('Ola', '12345@Ola')
+        self.driver.implicitly_wait(5)
+        book_page.click_add_to_collection_button()
+        WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+        self.driver.switch_to.alert.accept()
+        # Problem with click
+        book_page.click_user_profile()
+        book = book_page.find(By.ID, 'see-book-Git Pocket Guide')
+        assert book.is_displayed()
+
+    def test_check_search_books(self):
+        book_page = BookPage(self.driver)
+        book_page.open_page(TestData.book_url)
+        book_page.set_search_field('Git Pocket Guide')
+        book = book_page.find(By.ID, 'see-book-Git Pocket Guide')
+        assert book.text == 'Git Pocket Guide'
+        book_page.set_search_field('Lud')
+        assert book != True
+
+    def test_go_to_book_store(self):
+        book_page = BookPage(self.driver)
+        book_page.open_page(TestData.profile_url)
+        book_page.click_login_button_in_profile()
+        book_page.log_into_application('Ola', '12345@Ola')
+        # Problem with advertising?
+        book_page.click_button_go_to_store()
+        book_store = book_page.find(By.CLASS_NAME, 'main-header')
+        assert book_store.is_displayed()
+        assert book_store.text == 'Book Store'
+
+    def test_delete_book_from_profile(self):
+        book_page = BookPage(self.driver)
+        book_page.open_page(TestData.book_url)
+        book_page.click_book()
+        book_page.click_login_form()
+        book_page.log_into_application('Ola', '12345@Ola')
+        self.driver.implicitly_wait(5)
+        book_page.click_add_to_collection_button()
+        WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+        self.driver.switch_to.alert.accept()
+        # Problem with click
+        book_page.click_user_profile()
+        book = book_page.find(By.ID, 'see-book-Git Pocket Guide')
+        assert book.is_displayed()
+        book_page.click_delete_button()
+        book_page.click_close_small_model()
+        WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+        self.driver.switch_to.alert.accept()
+        assert book != True
 
 
-def test_check_search_books():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    browser.get('https://demoqa.com/books')
-    search_field = browser.find_element(By.ID, 'searchBox')
-    book_name = 'Git Pocket Guide'
-    search_field.send_keys(book_name)
-    book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
-    assert book.text == book_name
-    search_field = browser.find_element(By.ID, 'searchBox')
-    new_search = 'Lud'
-    search_field.send_keys(new_search)
-    assert book != True
 
 
-def test_delete_book_from_profile():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    browser.maximize_window()
-    browser.get('https://demoqa.com/books')
-    book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
-    book.click()
-    login_button = browser.find_element(By.ID, 'login')
-    login_button.click()
-    user_name = browser.find_element(By.ID, 'userName')
-    login = "Ola"
-    user_name.send_keys(login)
-    password = browser.find_element(By.ID, 'password')
-    passwordName = '12345@Ola'
-    password.send_keys(passwordName)
-    login_button = browser.find_element(By.ID, 'login')
-    login_button.click()
-    add_to_collection_button = browser.find_element(By.CSS_SELECTOR,
-                                                    'div[class = "text-right fullButton"] #addNewRecordButton')
-    add_to_collection_button.click()
-    WebDriverWait(browser, 10).until(EC.alert_is_present())
-    browser.switch_to.alert.accept()
-    user_profile = browser.find_element(By.CSS_SELECTOR, '.accordion :nth-child(6)>div>ul>li:nth-child(3)')
-    # Why do css-selektor work sometimes?
-    user_profile.click()
-    book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
-    assert book.is_displayed()
-    delete_button = browser.find_element(By.ID, 'delete-record-undefined')
-    delete_button.click()
-    browser.find_element(By.ID, 'closeSmallModal-ok').click()
-    WebDriverWait(browser, 10).until(EC.alert_is_present())
-    browser.switch_to.alert.accept()
-    assert book != True
 
 
-def test_go_to_book_store():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    browser.maximize_window()
-    browser.get('https://demoqa.com/profile')
-    browser.find_element(By.CSS_SELECTOR, '#notLoggin-label > a:nth-child(1)').click()
-    user_name = browser.find_element(By.ID, 'userName')
-    login = "Ola"
-    user_name.send_keys(login)
-    password = browser.find_element(By.ID, 'password')
-    passwordName = '12345@Ola'
-    password.send_keys(passwordName)
-    login_button = browser.find_element(By.ID, 'login')
-    login_button.click()
-    button_go_to_store = browser.find_element(By.ID, 'gotoStore')
-    button_go_to_store.click()
-    book_store = browser.find_element(By.CLASS_NAME, 'main-header')
-    assert book_store.is_displayed()
-    assert book_store.text == 'Book Store'
+
+# def test_add_book_into_your_profile():
+#     browser = webdriver.Chrome()
+#     browser.implicitly_wait(5)
+#     browser.maximize_window()
+#     browser.get('https://demoqa.com/books')
+#     book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
+#     book.click()
+#     login_button = browser.find_element(By.ID, 'login')
+#     login_button.click()
+#     user_name = browser.find_element(By.ID, 'userName')
+#     login = "Ola"
+#     user_name.send_keys(login)
+#     password = browser.find_element(By.ID, 'password')
+#     passwordName = '12345@Ola'
+#     password.send_keys(passwordName)
+#     login_button = browser.find_element(By.ID, 'login')
+#     login_button.click()
+#     add_to_collection_button = browser.find_element(By.CSS_SELECTOR,
+#                                                     'div[class = "text-right fullButton"] #addNewRecordButton')
+#     add_to_collection_button.click()
+#     WebDriverWait(browser, 10).until(EC.alert_is_present())
+#     browser.switch_to.alert.accept()
+#     user_profile = browser.find_element(By.CSS_SELECTOR, '.accordion :nth-child(6) > div > ul> li:nth-child(3)')
+#     browser.execute_script("arguments[0].scrollIntoView()", user_profile)
+#     user_profile.click()
+#     book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
+#     assert book.is_displayed()
+#
+#
+# def test_check_search_books():
+#     browser = webdriver.Chrome()
+#     browser.implicitly_wait(5)
+#     browser.get('https://demoqa.com/books')
+#     search_field = browser.find_element(By.ID, 'searchBox')
+#     book_name = 'Git Pocket Guide'
+#     search_field.send_keys(book_name)
+#     book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
+#     assert book.text == book_name
+#     search_field = browser.find_element(By.ID, 'searchBox')
+#     new_search = 'Lud'
+#     search_field.send_keys(new_search)
+#     assert book != True
+#
+#
+# def test_delete_book_from_profile():
+#     browser = webdriver.Chrome()
+#     browser.implicitly_wait(5)
+#     browser.maximize_window()
+#     browser.get('https://demoqa.com/books')
+#     book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
+#     book.click()
+#     login_button = browser.find_element(By.ID, 'login')
+#     login_button.click()
+#     user_name = browser.find_element(By.ID, 'userName')
+#     login = "Ola"
+#     user_name.send_keys(login)
+#     password = browser.find_element(By.ID, 'password')
+#     passwordName = '12345@Ola'
+#     password.send_keys(passwordName)
+#     login_button = browser.find_element(By.ID, 'login')
+#     login_button.click()
+#     add_to_collection_button = browser.find_element(By.CSS_SELECTOR,
+#                                                     'div[class = "text-right fullButton"] #addNewRecordButton')
+#     add_to_collection_button.click()
+#     WebDriverWait(browser, 10).until(EC.alert_is_present())
+#     browser.switch_to.alert.accept()
+#     user_profile = browser.find_element(By.CSS_SELECTOR, '.accordion :nth-child(6)>div>ul>li:nth-child(3)')
+#     # Why do css-selektor work sometimes?
+#     user_profile.click()
+#     book = browser.find_element(By.ID, 'see-book-Git Pocket Guide')
+#     assert book.is_displayed()
+#     delete_button = browser.find_element(By.ID, 'delete-record-undefined')
+#     delete_button.click()
+#     browser.find_element(By.ID, 'closeSmallModal-ok').click()
+#     WebDriverWait(browser, 10).until(EC.alert_is_present())
+#     browser.switch_to.alert.accept()
+#     assert book != True
+#
+#
+# def test_go_to_book_store():
+#     browser = webdriver.Chrome()
+#     browser.implicitly_wait(5)
+#     browser.maximize_window()
+#     browser.get('https://demoqa.com/profile')
+#     browser.find_element(By.CSS_SELECTOR, '#notLoggin-label > a:nth-child(1)').click()
+#     user_name = browser.find_element(By.ID, 'userName')
+#     login = "Ola"
+#     user_name.send_keys(login)
+#     password = browser.find_element(By.ID, 'password')
+#     passwordName = '12345@Ola'
+#     password.send_keys(passwordName)
+#     login_button = browser.find_element(By.ID, 'login')
+#     login_button.click()
+#     button_go_to_store = browser.find_element(By.ID, 'gotoStore')
+#     button_go_to_store.click()
+#     book_store = browser.find_element(By.CLASS_NAME, 'main-header')
+#     assert book_store.is_displayed()
+#     assert book_store.text == 'Book Store'
